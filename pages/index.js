c73a6197c8ae1f637e7688a36a80385d4cacdcf3
Patrick Gabriel M. Naguit <pgmnaguit.student@ua.edu.ph>
2026-09-17 11:51:48 +0800

    Initial Next.js blog

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+
+      <section className={utilStyles.headingMd}>
+
+        <h3>Why I Chose My IT Specialization</h3>
+        <p>
+          I chose Web and Mobile Development because I am interested in
+          creating websites and applications that can solve real-world
+          problems. I enjoy learning how technology can make information and
+          services easier to access. The growing use of mobile applications
+          and web-based systems in businesses and everyday life inspired me
+          to pursue this specialization.
+        </p>
+
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
