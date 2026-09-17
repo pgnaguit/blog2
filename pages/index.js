954a67acb780b5b4d4706a20885b7388f82fd9a5
Patrick Gabriel M. Naguit <pgmnaguit.student@ua.edu.ph>
2026-09-17 12:02:23 +0800

    Update profile introduction

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,15 @@ export default function Home() {
 
         <h3>Why I Chose My IT Specialization</h3>
         <p>
-          I chose Web and Mobile Development because I am interested in
-          creating websites and applications that can solve real-world
-          problems. I enjoy learning how technology can make information and
-          services easier to access. The growing use of mobile applications
-          and web-based systems in businesses and everyday life inspired me
-          to pursue this specialization.
+            I chose Web and Mobile Development because I am interested in creating
+            websites and applications that can solve real-world problems. I enjoy
+            learning how technology can make information and services easier to
+            access. The growing use of mobile applications and web-based systems
+            in businesses and everyday life inspired me to pursue this
+            specialization. I also want to learn how to design and develop
+            applications that are useful and user-friendly. Through this
+            specialization, I hope to improve my technical skills and create
+            meaningful digital solutions in the future.
         </p>
 
         <p>
